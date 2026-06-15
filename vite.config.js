f4a9cb4ba16edc51c8974a99490788ec66036ebd
Saraheve29 <sarahwhyte33@gmail.com>
2026-06-15 14:20:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2015', 'chrome58', 'firefox57', 'safari11'],
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
